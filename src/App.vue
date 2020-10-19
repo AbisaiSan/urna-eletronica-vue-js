0b70194d7b6d9ce391bc7eaea0b0db73f8ea1b4d
AbisaiSan <abisaiweb7@gmail.com>
2020-10-19 11:58:13 -0300

    melhorando os componentes

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,27 @@
 <template>
-  
+  <div id="app">
+    <app-urna/>
+    <app-tela/>
+    <app-teclado/> 
+  </div>
 </template>
 
 <script>
-
+import Urna from './components/views/Urna'
+import Tela from './components/views/Tela'
+import Teclado from './components/views/Teclado'
 
 export default {
-  
+components: {'app-urna':Urna,'app-tela':Tela,'app-teclado':Teclado},
+data(){
+return {
+			componente:{Urna,Tela,Teclado}
+		}
+	}
 }
+
 </script>
 
 <style >
-
-</style>>
-
 
 </style>
